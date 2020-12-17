ccacd8c0b46d44326f8d4b35673ed020051f690b
Roman Mashta <roman.mashta@gmail.com>
2020-12-17 16:54:35 +0200

    Localisation settings

---
+++ BotConfiguration.cs
@@ -0,0 +1,14 @@
+using Cherry.Lib.Core.Localization;
+
+namespace Cherry.Lib.Core.App
+{
+    public class BotConfiguration: LocalConfiguration
+    {
+        public static string SectionName = "Bot";
+
+        public bool UseRegistration { get; set; } = false;
+        public bool NeedApproval { get; set; } = false;
+        public string BucketName { get; set; }
+        public string MenuMessage { get;set; }
+    }
+}
