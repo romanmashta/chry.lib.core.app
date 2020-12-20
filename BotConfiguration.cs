50ef0019c81b9c98ceb85999b7cab2880069a6a8
Roman Mashta <roman.mashta@gmail.com>
2020-12-20 18:31:41 +0200

    NoMenu option added

--- BotConfiguration.cs
+++ BotConfiguration.cs
@@ -2,13 +2,15 @@ using Cherry.Lib.Core.Localization;
 
 namespace Cherry.Lib.Core.App
 {
-    public class BotConfiguration: LocalConfiguration
+    public class BotConfiguration : LocalConfiguration
     {
         public static string SectionName = "Bot";
 
         public bool UseRegistration { get; set; } = false;
         public bool NeedApproval { get; set; } = false;
         public string BucketName { get; set; }
-        public string MenuMessage { get;set; }
+        public string MenuMessage { get; set; }
+        public bool NoMenu { get; set; }
+        public bool ResetOnStart { get; set; }
     }
 }
